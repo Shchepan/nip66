0287e2db88b595c5a118528ada1509131fc4d9c7
Shchepan <83298020+Shchepan@users.noreply.github.com>
2026-05-12 13:13:18 +0200

    Update manager.py

--- src/manager.py
+++ src/manager.py
@@ -1,94 +1,103 @@
-from src.models import Apartment, Bill, Parameters, Tenant, TenantSettlement, Transfer, ApartmentSettlement
-from typing import List, Tuple
+from pydantic import BaseModel
+from typing import Dict, List
 
-class Manager:
-    def __init__(self, parameters: Parameters):
-        self.parameters = parameters 
+import json
 
-        self.apartments = {}
-        self.tenants = {}
-        self.transfers = []
-        self.bills = []
-       
-        self.load_data()
 
-    def load_data(self):
-        self.apartments = Apartment.from_json_file(self.parameters.apartments_json_path)
-        self.tenants = Tenant.from_json_file(self.parameters.tenants_json_path)
-        self.transfers = Transfer.from_json_file(self.parameters.transfers_json_path)
-        self.bills = Bill.from_json_file(self.parameters.bills_json_path)
+class Parameters(BaseModel):
+    apartments_json_path: str = 'data/apartments.json'
+    tenants_json_path: str = 'data/tenants.json'
+    transfers_json_path: str = 'data/transfers.json'
+    bills_json_path: str = 'data/bills.json'
 
-    def check_tenants_apartment_keys(self) -> bool:
-        for tenant in self.tenants.values():
-            if tenant.apartment not in self.apartments:
-                return False
-        return True
 
-    def get_apartment_costs(self, apartment_key: str, year: int = None, month: int = None) -> float | None:
-        if month is not None and (month < 1 or month > 12):
-            raise ValueError("Month must be between 1 and 12")
-        if apartment_key not in self.apartments:
-            return None
-        total_cost = 0.0
-        for bill in self.bills:
-            if bill.apartment == apartment_key and (year is None or bill.settlement_year == year) and (month is None or bill.settlement_month == month):
-                total_cost += bill.amount_pln
-        return total_cost
+class Room(BaseModel):
+    name: str
+    area_m2: float
 
-    def get_settlement(self, apartment_key: str, year: int, month: int) -> ApartmentSettlement | None:
-        if month < 1 or month > 12:
-            raise ValueError("Month must be between 1 and 12")
-        if apartment_key not in self.apartments:
-            return None
-        total_cost = self.get_apartment_costs(apartment_key, year, month)
-        if total_cost is None:
-            return None
-        
-        return ApartmentSettlement(
-            key=f"{apartment_key}-{year}-{month}",
-            apartment=apartment_key,
-            year=year,
-            month=month,
-            total_due_pln=total_cost
-        )
+
+class Apartment(BaseModel):
+    key: str
+    name: str
+    location: str
+    area_m2: float
+    rooms: Dict[str, Room]
+
+    @staticmethod
+    def from_json_file(file_path: str) -> Dict[str,'Apartment']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, dict), "Expected a dictionary of apartments"
+        return {key: Apartment(**apartment) for key, apartment in data.items()}
+
     
-    def create_tenants_settlements(self, apartment_settlement: ApartmentSettlement) -> List[TenantSettlement] | None:
-        if apartment_settlement.month < 1 or apartment_settlement.month > 12:
-            raise ValueError("Month must be between 1 and 12")
-        if apartment_settlement.apartment not in self.apartments:
-            return None
-        tenants_in_apartment = [tenant for tenant in self.tenants.values() if tenant.apartment == apartment_settlement.apartment]
-        if not tenants_in_apartment:
-            return []
-        
-        return [
-            TenantSettlement(
-                tenant=tenant.name,
-                apartment_settlement=apartment_settlement.key,
-                month=apartment_settlement.month,
-                year=apartment_settlement.year,
-                total_due_pln=apartment_settlement.total_due_pln / len(tenants_in_apartment)
-            )
-        for tenant in tenants_in_apartment ] 
-    
-    def get_tax(self, year: int, month: int, tax_rate: float) -> int:
-        """Zwraca sumę przychodów wymnożoną przez stawkę podatku (zaokrągloną do pełnych zł)"""
-        total_income = sum(
-            transfer.amount_pln for transfer in self.transfers 
-            if transfer.settlement_year == year and transfer.settlement_month == month
-        )
-        return round(total_income * tax_rate)
+class Tenant(BaseModel):
+    name: str
+    apartment: str
+    room: str
+    rent_pln: float
+    deposit_pln: float
+    date_agreement_from: str
+    date_agreement_to: str
 
-    def find_apartments_without_bills(self, year: int, month: int) -> list:
-        """Zwraca listę kluczy mieszkań, które nie mają żadnego rachunku w danym miesiącu"""
-        missing_apartments = []
-        for apartment_key in self.apartments:
-            
-            has_bills = any(
-                bill.apartment == apartment_key and bill.settlement_year == year and bill.settlement_month == month 
-                for bill in self.bills
-            )
-            if not has_bills:
-                missing_apartments.append(apartment_key)
-                
-        return missing_apartments
+    @staticmethod
+    def from_json_file(file_path: str) -> Dict[str,'Tenant']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, dict), "Expected a dictionary of tenants"
+        return {key: Tenant(**tenant) for key, tenant in data.items()}
+    
+
+class Transfer(BaseModel):
+    amount_pln: float
+    date: str
+    settlement_year: int | None
+    settlement_month: int | None
+    tenant: str
+
+    @staticmethod
+    def from_json_file(file_path: str) -> List['Transfer']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, list), "Expected a list of transfers"
+        return [Transfer(**transfer) for transfer in data]
+
+
+class Bill(BaseModel):
+    amount_pln: float
+    date_due: str
+    apartment: str
+    settlement_year: int
+    settlement_month: int
+    type: str
+
+    @staticmethod
+    def from_json_file(file_path: str) -> List['Bill']:
+        data = None
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        assert isinstance(data, list), "Expected a list of bills"
+        return [Bill(**bill) for bill in data]
+
+
+class ApartmentSettlement(BaseModel):
+    key: str
+    apartment: str
+    month: int
+    year: int
+    total_due_pln: float
+    total_transfers_pln: float = 0.0
+    balance_pln: float = 0.0
+
+
+class TenantSettlement(BaseModel):
+    tenant: str
+    apartment_settlement: str
+    month: int
+    year: int
+    total_due_pln: float
+    total_transfers_pln: float = 0.0
+    balance_pln: float = 0.0
